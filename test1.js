32aea227d85cbd44ab211d23c10511659683764c
chinmayamishra039 <mishrasanu250@gmail.com>
2022-11-22 11:53:48 +0530

    demo2 Commiting

--- test1.js
+++ test1.js
@@ -2,4 +2,5 @@ console.log("This is a test");
 console.log("This is second test");
 console.log("This msg is from the server");
 console.log("Hi Alok remote change");
-console.log("Working fine ");
+console.log("Working fine2 ");
+console.log("This is from Demo2");
