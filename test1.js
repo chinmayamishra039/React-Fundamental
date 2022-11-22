b5e268c4fad774799704a5db12526740e351eb67
chinmayamishra039 <mishrasanu250@gmail.com>
2022-11-22 10:05:48 +0530

    alok is added

--- test1.js
+++ test1.js
@@ -1,3 +1,4 @@
 console.log("This is a test");
 console.log("This is second test");
-console.log("This msg is from the server")
+console.log("This msg is from the server");
+console.log("Hi Alok remote change");
